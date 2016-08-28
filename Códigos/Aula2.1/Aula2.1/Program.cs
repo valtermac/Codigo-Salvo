af224b3219e0be0e753b3418706e554d6a59bdb8
Thiago Bucalon <thiago_rodrigo90@hotmail.com>
2016-08-27 22:38:05 -0300

    Aula 2 - Códigos
    Códigos da Aula 2.
    2.1  - exemplo de tipo de variável
    2.2 - calcular área de um quadrado

---
+++ Códigos/Aula2.1/Aula2.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // VARIÁVEIS
+            /*TIPOS DE DADOS NUMÉRICOS */
+
+            int numerointeiro;
+            double numeroFracionado1;
+            float numeroFracionado2;
+
+            char umCaracter;
+            string variosCaracteres;
+
+            bool verdadeiro;;
+            bool falso;
+
+            //INÍCIO
+            numerointeiro = 3;
+            numeroFracionado1 = 3.14;
+            numeroFracionado2 = 0.4f;
+
+            umCaracter = 'a';
+            variosCaracteres = "Código Salvo";
+
+            verdadeiro = true;
+            falso = false;
+
+            //operadores
+
+            numerointeiro = 3 + 10;
+            numeroFracionado1 = 5 - numeroFracionado1;
+            numeroFracionado2 = 7 * 5.5f;
+            numeroFracionado1 = 20 / 2;
+
+
+            //FIM
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Códigos/Aula2.2/Aula2.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIÁVEIS
+
+            int lado;
+            int altura;
+            int areaQuadrado;
+
+            //INÍCIO
+
+            Console.Write("Digite o valor da lado do quadrado: ");      /*entradas*/
+            lado = Convert.ToInt32(Console.ReadLine());                 /*entradas*/
+            Console.Write("Digite o valor da altura do quadrado: ");      /*entradas*/
+            altura = Convert.ToInt32(Console.ReadLine());               /*entradas*/
+                        
+            areaQuadrado = lado * altura;                               /*processamento*/
+            
+            Console.WriteLine("Valor da área é: {0}", areaQuadrado);    /*saída*/
+
+            //FIM
+            Console.ReadKey();
+        }
+    }
+}
